94174a210c8c26e9d6a7e60220451d302f2d0da2
Mateus Barbosa <77073090+MateusBarbosa1@users.noreply.github.com>
2024-02-16 01:53:50 +0000

    Finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,31 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result.rows);
+  const updateAt = new Date().toISOString();
+
+  const versionPostgres = await database.query("show server_version;");
+  const versionPostgresValue = versionPostgres.rows[0]["server_version"];
+
+  const maxConnections = await database.query("show max_connections;");
+  const databaseMaxConnectionsValue = maxConnections.rows[0]["max_connections"];
+
+  const databaseName = process.env.POSTGRES_DB; // QUERYS DINAMICAS
+  const openedConnectios = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseOpenedConnectiosValue = openedConnectios.rows[0].count;
+
   response.status(200).json({
-    msg: "Rota ativa!",
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        postgres_version: parseFloat(versionPostgresValue),
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: parseInt(databaseOpenedConnectiosValue),
+      },
+    },
   });
 }
 
--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -1,4 +1,23 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  // VERIFICA SE A DATA NÂO ESTA VAZIA
+  const responseBody = await response.json();
+
+  const expectedDate = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(expectedDate);
+
+  // VERIFICA SE AS INFORMAÇÕES DO BANCO DE DADOS ESTA NO TIPO CORRETO
+  expect(responseBody.dependencies.database.postgres_version).toEqual(16.2);
+  expect(typeof responseBody.dependencies.database.postgres_version).toBe(
+    "number",
+  );
+
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(typeof responseBody.dependencies.database.max_connections).toBe(
+    "number",
+  );
+
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
